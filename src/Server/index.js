4924797b623b5983656918582797724051ccf3e7
Mauri20 <pr0164032019@unab.edu.sv>
2023-03-28 09:38:21 -0600

    Agregando encabezados CORS para permitir acceso

--- src/Server/index.js
+++ src/Server/index.js
@@ -6,6 +6,12 @@ import { initializeDB } from './db';
 const { port } = getConfig();
 
 const app = express();
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 // creating Server
 const initializeServer = async (routes) => {
